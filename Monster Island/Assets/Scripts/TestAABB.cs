6f84c2a8b92dec2d5a7b7628f116f5d65ba3f35c
TheExpandingPolytope <jesseengerman@hotmail.com>
2023-01-21 05:33:54 -0600

    obstacles and player impl

--- Monster Island/Assets/Scripts/TestAABB.cs
+++ Monster Island/Assets/Scripts/TestAABB.cs
@@ -4,8 +4,10 @@ using UnityEngine;
 
 public class TestAABB : MonoBehaviour
 {
-    public BoxCollider2D a;
-    public BoxCollider2D b;
+    public BoxCollider2D player;
+    public BoxCollider2D[] obstacles;
+    public float speed = 1;
+
     
     // Start is called before the first frame update
     void Start()
@@ -17,11 +19,36 @@ public class TestAABB : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.T)){
-            var isCollision = IsAABB(a, b);
-            Debug.Log($"is collided: {isCollision}");
+        Vector3 v = new Vector3(0.0f, 0.0f, 0.0f);
+        if(Input.GetKeyDown(KeyCode.A)){
+            Move(Vector3.left * speed);    
+        }
+        if(Input.GetKeyDown(KeyCode.W)){
+            Move(Vector3.up * speed);
+        }
+        if(Input.GetKeyDown(KeyCode.S)){
+            Move(Vector3.down * speed);
+        }
+        if(Input.GetKeyDown(KeyCode.D)){
+            Move(Vector3.right * speed);
         }
         
+    }
+
+    void Move(Vector3 velocity) {
+        if(!WillCollide(velocity)){
+            player.transform.localPosition += velocity;
+        }
+    }
+
+    bool WillCollide(Vector3 velocity){
+        for (int i = 0; i <= obstacles.Length - 1; i = i + 1) 
+        {
+            BoxCollider2D obstacle = obstacles[i];
+            if(player.OverlapPoint(obstacle.transform.position-velocity))
+                return true;
+        }
+        return false;
     }
 
     bool IsAABB(BoxCollider2D a, BoxCollider2D b){
